7d48db80abb9ca664b9b09653168ab500848b5f2
Denise Nanni <denise_n@hotmail.it>
2020-07-28 16:37:56 +0200

    default route fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { ResumeComponent } from './resume/resume.component';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '', 
     component: AppComponent,
     children: [
+      { path: 'home', component: HomeComponent },
       { path: 'resume', component: ResumeComponent },
       { path: 'contact-form', component: ContactFormComponent },
     ]
   },
-  { path: '',   redirectTo: '/', pathMatch: 'full' },
   { path: '**', component: AppComponent },
 ];
 
