5db6646a1fb054e96823aeb300819efea4775ed9
Denise Nanni <denise_n@hotmail.it>
2020-07-29 16:00:18 +0200

    articles card added to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleCard } from '../model/article';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  articleCards: ArticleCard[];
+
+  constructor() {
+    this.articleCards = [];
+   }
 
   ngOnInit(): void {
   }
