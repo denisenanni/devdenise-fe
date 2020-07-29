64d0f50ae3a3669f59764df95456bba609e1ff11
Denise Nanni <denise_n@hotmail.it>
2020-07-29 16:00:58 +0200

    article-card and article component added

---
+++ src/app/article-card/article-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'article-card',
+  templateUrl: './article-card.component.html',
+  styleUrls: ['./article-card.component.scss'],
+})
+export class ArticleCardComponent implements OnInit {
+
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  goToArticle(i: number) {}
+}
