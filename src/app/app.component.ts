db38612f25cf1d945a08615837b4810acd2adebd
Denise Nanni <denise_n@hotmail.it>
2020-07-30 10:56:03 +0200

    docs/index modified and app-component menu style fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   constructor(private router: Router) {}
   title = 'DevDenise';
   linkedinProfile = 'https://www.linkedin.com/in/denise-nanni/';
   gitHubProfile = 'https://github.com/DeniseN90';
-  customAction: 'RESUME';
-
 
 }
