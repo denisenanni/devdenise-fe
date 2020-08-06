dfc5c150193fe18d1ded3e269a53fcaef866f1b5
Denise Nanni <denise_n@hotmail.it>
2020-08-06 11:23:23 +0200

    home modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ArticleCard } from '../model/article';
+import { ArticleCard, Article } from '../model/article';
+
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,14 @@ import { ArticleCard } from '../model/article';
 export class HomeComponent implements OnInit {
 
   articleCards: ArticleCard[];
+  articleCard: ArticleCard;
+  title = "How to set up a WordPress site on an EC2 AWS instance with an SSL certificate";
+  text = "With AWS it is possible to deploy a WordPress site within minutes";
+
 
   constructor() {
     this.articleCards = [];
+    this.articleCard = new ArticleCard(); 
    }
 
   ngOnInit(): void {
