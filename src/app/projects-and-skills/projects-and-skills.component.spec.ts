c629bc2870b90deacd2aac9d21355909222478b7
Denise Nanni <denise_n@hotmail.it>
2020-08-18 16:11:06 +0200

    projects and skills component added

---
+++ src/app/projects-and-skills/projects-and-skills.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectsAndSkillsComponent } from './projects-and-skills.component';
+
+describe('ProjectsAndSkillsComponent', () => {
+  let component: ProjectsAndSkillsComponent;
+  let fixture: ComponentFixture<ProjectsAndSkillsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProjectsAndSkillsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectsAndSkillsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/projects-and-skills/projects-and-skills.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'projects-and-skills',
+  templateUrl: './projects-and-skills.component.html',
+  styleUrls: ['./projects-and-skills.component.scss']
+})
+export class ProjectsAndSkillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
