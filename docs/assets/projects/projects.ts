55ce6a0eb49ac1e01dbd20a33ac783eb3e3c029b
denisenanni <denisenanni@users.noreply.github.com>
2023-02-28 11:24:12 +0000

    deploy: 8d811fe9f0fd2998dcc17d7cc17f2a02e4bd57b8

---
+++ docs/assets/projects/projects.ts
@@ -0,0 +1,25 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Leader Clinique",
+        link: "https://leaderclinique.com/",
+        imageLink: "https://devdenise.s3.eu-west-3.amazonaws.com/leaderclinique.JPG",
+        description: "This static site is hosted on AWS S3 and the form is managed through a Google script. The technology used are HTML, CSS and Javascript.",
+        
+    },
+    {
+        id: 2,
+        title: "CovidStats",
+        link: "https://denisenanni.github.io/covid-stats/",
+        imageLink: "https://devdenise.s3.eu-west-3.amazonaws.com/covid-stats.JPG",
+        description: "Angular application hosted on GitHub Pages, with a CD/CI pipleine integration."
+    },
+    {
+        id: 3,
+        title: "Pubbliufficio",
+        link: "https://pubbliufficio.com/",
+        imageLink: "https://devdenise.s3.eu-west-3.amazonaws.com/pubbliufficio.jpg",
+        description: "React application hosted on GitHub Pages, with a custom domain and a form managed by a Google script."
+
+    }
+]
